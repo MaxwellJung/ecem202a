5b6564f2b644eee8d3ba18cebd6c1462c73822fa
Maxwell Jung <jungmaxwell@gmail.com>
2025-10-14 00:54:18 -0700

    feat: generate alignment matrix

---
+++ src/align-mat.py
@@ -0,0 +1,33 @@
+"""Given vector c and y, generate alignment matrix
+
+Usage:
+    python3 ./src/align-mat.py
+"""
+
+import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
+import matplotlib.pyplot as plt
+
+def main():
+    y = np.load('out/y.npy')
+    c = np.load('out/c.npy')
+
+    window_size = 1024
+    c_prime_windows = sliding_window_view(c, window_size)
+    y_windows = sliding_window_view(y, window_size)
+
+    print(np.info(c_prime_windows))
+    print(np.info(y_windows))
+
+    x = np.dot(c_prime_windows, y_windows.T)
+    print(np.info(x))
+
+    fig = plt.figure(figsize=(16, 9))
+    plt.matshow(x)
+    plt.title("Alignment Matrix")
+    plt.xlabel("Y index")
+    plt.ylabel("C index")
+    plt.savefig("out/align-mat.png")
+
+if __name__ == '__main__':
+    main()
