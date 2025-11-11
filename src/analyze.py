a3ee33acd6af13ffa16b4c548eb4650850229c9f
Maxwell Jung <jungmaxwell@gmail.com>
2025-11-10 22:09:52 -0800

    feat: working alignment matrix on c_paper.mov
    commit 3f040c43bb2a76cd1f121adc63597da185be49a6
    Author: Maxwell Jung <jungmaxwell@gmail.com>
    Date:   Mon Nov 10 22:09:11 2025 -0800
    
        feat: improve plots
    
    commit bd0848581f11faf78fc815268f131ef38d126e5e
    Author: Maxwell Jung <jungmaxwell@gmail.com>
    Date:   Mon Nov 10 14:24:24 2025 -0800
    
        test: working alignment matrix on y.mov
    
    commit f36951614120a2aaddcf7e0f2636b086ab5aa830
    Author: Maxwell Jung <jungmaxwell@gmail.com>
    Date:   Mon Nov 10 13:49:43 2025 -0800
    
        test: working alignment matrix on c_paper.mov
    
    commit 7c81a82572ffe70cb7db189e78bfdce3bf5a8ddb
    Author: Maxwell Jung <jungmaxwell@gmail.com>
    Date:   Sun Nov 9 23:41:33 2025 -0800
    
        feat: record video illuminated by NCI monitor

--- src/analyze.py
+++ src/analyze.py
@@ -8,8 +8,8 @@ import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
 import matplotlib.pyplot as plt
 import torch
-import time
 import cv2
+from scipy.signal import resample
 
 # Check if GPU is available (CUDA for NVIDIA, MPS for Apple Silicon)
 if torch.cuda.is_available():
@@ -21,55 +21,188 @@ else:
 print(f"Using device: {device}")
 
 def main():
-    c = np.load('out/c.npy')
-    y = np.load('out/y.npy')
-    VIDEO_LENGTH = y.shape[0]
-    VIDEO_WIDTH = y.shape[1]
-    VIDEO_HEIGHT = y.shape[2]
+    C_ARRAY_FILE = 'in/irl/c.npy'
+    C_SAMPLE_RATE = 30
 
-    # Simulate malicious video cut
-    y = np.concatenate((y[:VIDEO_LENGTH//4], y[2*VIDEO_LENGTH//4:]))
-    # Simulate malicious photoshop (edit in a grey square in the middle)
-    y[0:VIDEO_LENGTH//8, VIDEO_WIDTH//4:VIDEO_WIDTH//2, VIDEO_HEIGHT//4:VIDEO_HEIGHT//2] = 0.5
+    Y_VIDEO_FILE = 'in/irl/iphone/c_paper.mov'
+    VIDEO_FPS = 30
+    Y_SAMPLE_RATE = VIDEO_FPS
+
+    c = np.load(C_ARRAY_FILE)
+    y = load_video(Y_VIDEO_FILE)
+
+    # downscale video
+    DOWNSCALE_FACTOR = 4
+    y = y[:, ::DOWNSCALE_FACTOR, ::DOWNSCALE_FACTOR, :]
+
+    VIDEO_LENGTH = y.shape[0]
+    VIDEO_HEIGHT = y.shape[1]
+    VIDEO_WIDTH = y.shape[2]
+
+    # match c's sample rate to y
+    C_SAMPLE_RATE = int(len(c)*(Y_SAMPLE_RATE/C_SAMPLE_RATE))/len(c)*C_SAMPLE_RATE
+    print(f'Resampling c to {C_SAMPLE_RATE} Hz')
+    c = resample(c, int(len(c)*(Y_SAMPLE_RATE/C_SAMPLE_RATE)))
+
+    # linear gamma correction
+    GAMMA = 2.2
+    print(f'Applying gamma={GAMMA} correction')
+    y = (y/255)**(1/GAMMA)
+
+    # # Simulate malicious video cut
+    # y = np.concatenate((y[:VIDEO_LENGTH//4], y[2*VIDEO_LENGTH//4:]))
+    # # Simulate malicious photoshop (edit in a grey square in the middle)
+    # y[0:VIDEO_LENGTH//8, VIDEO_WIDTH//4:VIDEO_WIDTH//2, VIDEO_HEIGHT//4:VIDEO_HEIGHT//2] = 0.5
 
     # export edited video
-    VIDEO_FPS = 30
-    out = cv2.VideoWriter('out/y_edited.mp4', cv2.VideoWriter_fourcc(*'mp4v'), VIDEO_FPS, (VIDEO_HEIGHT, VIDEO_WIDTH), True)
-    for frame in (255*y).clip(min=0, max=255):
-        out.write(frame.astype(np.uint8))
-    out.release()
+    # VIDEO_FPS = 30
+    # out = cv2.VideoWriter('out/y_edited.mp4', cv2.VideoWriter_fourcc(*'mp4v'), VIDEO_FPS, (VIDEO_HEIGHT, VIDEO_WIDTH), True)
+    # for frame in (255*y).clip(min=0, max=255):
+    #     out.write(frame.astype(np.uint8))
+    # out.release()
 
     print(f"{y.shape=}")
     print(f"{c.shape=}")
 
-    start_time = time.time()
+    ###############################################################################
+    # Plots for debugging
+    ###############################################################################
+
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.add_subplot()
+    ax.imshow(y[0])
+    ax.set_title("Y Frame 0")
+    ax.set_xlabel("Width")
+    ax.set_ylabel("Height")
+    fig.savefig("out/y_frame0.png")
+    print("Saved plot out/y_frame0.png")
+    plt.close(fig)
+
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.add_subplot()
+    ax.imshow(y[1])
+    ax.set_title("Y Frame 1")
+    ax.set_xlabel("Width")
+    ax.set_ylabel("Height")
+    fig.savefig("out/y_frame1.png")
+    print("Saved plot out/y_frame1.png")
+    plt.close(fig)
+
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.add_subplot()
+    ax.plot(y[:, VIDEO_HEIGHT//2, VIDEO_WIDTH//2], '.')
+    ax.set_title(f"Pixel({VIDEO_WIDTH//2},{VIDEO_HEIGHT//2}) Intensity")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Pixel Intensity")
+    fig.savefig("out/y.png")
+    print("Saved plot out/y.png")
+    plt.close(fig)
+
+    # fig = plt.figure(figsize=(16, 9))
+    # ax = fig.add_subplot()
+    # ax.hist(y.flatten(), bins=100)
+    # ax.set_title("Y Distribution")
+    # ax.set_xlabel("Pixel Intensity")
+    # ax.set_ylabel("Count")
+    # fig.savefig("out/y_histogram.png")
+    # plt.close(fig)
+
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.add_subplot()
+    ax.step(np.arange(len(c))/C_SAMPLE_RATE, c, where='post')
+    ax.set_title("Coded Light Signal")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Amplitude")
+    fig.savefig("out/c.png")
+    print("Saved plot out/c.png")
+    plt.close(fig)
+
+    # fig = plt.figure(figsize=(16, 9))
+    # ax = fig.add_subplot()
+    # plt.hist(c, bins=100)
+    # ax.set_title("C Distribution")
+    # ax.set_xlabel("Amplitude")
+    # ax.set_ylabel("Count")
+    # fig.savefig("out/c_histogram.png")
+    # plt.close(fig)
+
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.add_subplot()
+    ax.step(C_SAMPLE_RATE*np.arange(len(c))/len(c), np.abs(np.fft.fft(c)), where='mid')
+    ax.set_title("Magnitude Spectrum of C")
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_ylabel("Magnitude")
+    fig.savefig("out/c_spectrum_magnitude.png")
+    print("Saved plot out/c_spectrum_magnitude.png")
+    plt.close(fig)
+
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.add_subplot()
+    ax.step(C_SAMPLE_RATE*np.arange(len(c))/len(c), np.angle(np.fft.fft(c)), where='mid')
+    ax.set_title("Phase Spectrum of C")
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_ylabel("Phase")
+    fig.savefig("out/c_spectrum_phase.png")
+    print("Saved plot out/c_spectrum_phase.png")
+    plt.close(fig)
+
+    ###############################################################################
+    # Start analysis
+    ###############################################################################
+
     align_mat = get_alignment_matrix(y, c)
-    elapsed_time = time.time() - start_time
+    y_to_c = align_mat.argmax(axis=0)
+    c_index_start = np.min(y_to_c)
+    c_index_end = np.max(y_to_c)+1
+    cropped_align_mat = align_mat[c_index_start:c_index_end]
 
     fig = plt.figure(figsize=(16, 9))
-    plt.matshow(align_mat)
-    plt.title("Alignment Matrix")
-    plt.xlabel("Y index")
-    plt.ylabel("C index")
-    plt.savefig("out/align-mat.png")
-    plt.close()
-    print("Saved alignment matrix to out/align-mat.png")
+    ax = fig.add_subplot()
+    ax.matshow(cropped_align_mat, origin='lower', extent=[(0-0.5)/Y_SAMPLE_RATE, (len(y)-0.5)/Y_SAMPLE_RATE, (c_index_start-0.5)/C_SAMPLE_RATE, (c_index_end-0.5)/C_SAMPLE_RATE])
+    ax.set_title("Alignment Matrix (Cropped)")
+    ax.xaxis.tick_bottom()
+    ax.set_xlabel("Y time (second)")
+    ax.set_ylabel("C time (second)")
+    fig.savefig("out/align-mat.png")
+    print("Saved alignment matrix out/align-mat.png")
+    plt.close(fig)
 
-    r = calculate_r(y, c)
-    fig = plt.figure(figsize=(16, 9))
-    plt.imshow(r[0])
-    plt.title("Code Image")
-    plt.xlabel("Width")
-    plt.ylabel("Height")
-    plt.savefig("out/r_estimate.png")
-    plt.close()
-    print("Saved code image to out/r_estimate.png")
+    # r = calculate_r(y, c)
+    # fig = plt.figure(figsize=(16, 9))
+    # ax.imshow(r[0])
+    # ax.set_title("Code Image")
+    # ax.set_xlabel("Width")
+    # ax.set_ylabel("Height")
+    # fig.savefig("out/r_estimate.png")
+    # plt.close(fig)
+    # print("Saved code image to out/r_estimate.png")
 
-    out = cv2.VideoWriter('out/r_estimate.mp4', cv2.VideoWriter_fourcc(*'mp4v'), VIDEO_FPS, (VIDEO_HEIGHT, VIDEO_WIDTH), True)
-    for frame in (255*r).clip(min=0, max=255):
-        out.write(frame.astype(np.uint8))
-    out.release()
-    print("Saved code video to out/r_estimate.mp4")
+    # out = cv2.VideoWriter('out/r_estimate.mp4', cv2.VideoWriter_fourcc(*'mp4v'), VIDEO_FPS, (VIDEO_HEIGHT, VIDEO_WIDTH), True)
+    # for frame in (255*r).clip(min=0, max=255):
+    #     out.write(frame.astype(np.uint8))
+    # out.release()
+    # print("Saved code video to out/r_estimate.mp4")
+
+
+def load_video(video_path):
+    print(f'Loading video file {video_path}')
+    cap = cv2.VideoCapture(video_path)
+
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    y = np.empty((frame_count, frame_height, frame_width, 3), np.dtype('uint8'))
+
+    for i in range(frame_count):
+        try:
+            _, y[i] = cap.read()
+        except TypeError as e:
+            pass
+
+    cap.release()
+
+    return y
 
 
 def get_alignment_matrix(y, c, window_size=511):
